58e667fcbce9c5742530bc478bb840cf00737466
Dima Shirkov <dimashirkov@list.ru>
2022-05-25 18:44:27 +0300

    Dak1 draft v2

--- task.js
+++ task.js
@@ -1,18 +1,20 @@
 // Задание1 появление окна на сайте
-const popUpWindow = document.getElementById('modal_main');
-popUpWindow.className = 'modal modal_active';
+const popUpWindow = document.getElementById('modal_main'); //определение модального окна
+popUpWindow.className = 'modal modal_active'; // активация модального окна
 
 // Задание2,3
-let closing = Array.from(document.getElementsByClassName('modal__close'));
+const closing = Array.from(document.getElementsByClassName('modal__close')); // массив с элементами имеющими класс модальное закрытие
+const modalSuccess = document.getElementById('modal_success'); //определение модального окна modal_success
+const activeWindows = Array.from(document.getElementsByClassName('modal')); // массив с элементами у которых класс modal
 
-closing.forEach((index) => {
-    if (index.className.includes('show-success')){
-        let modalSuccess = document.getElementById('modal_success');
+
+closing.forEach((index) => { // итерация по массиву closing
+    if (index.className.includes('show-success')){ // содержит ли элемент с классом show-success
         index.onclick = function() {
+            popUpWindow.classList.remove('modal_active'); // удаление класса modal_active у предыдущего окна 
             modalSuccess.classList.add('modal_active');
         };
     } else {
-        let activeWindows = Array.from(document.getElementsByClassName('modal'));
         index.onclick = function() {
             activeWindows.forEach((i) => i.classList.remove('modal_active'));
         };
