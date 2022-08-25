75f04b62f6271551fe7f972cc7eda8cfdb904d1f
Dima Shirkov <dimashirkov@list.ru>
2022-08-25 08:27:21 +0300

    dak1 draft v4

--- task.js
+++ task.js
@@ -4,24 +4,33 @@ const closing = Array.from(document.getElementsByClassName('modal__close')); // 
 const modalSuccess = document.getElementById('modal_success'); //определение модального окна modal_success
 const activeWindows = Array.from(document.getElementsByClassName('modal')); // массив с элементами у которых класс modal
 
-if (localStorage.modal != 'inactive') {
-    popUpWindow.className = 'modal modal_active'; // активация модального окна
+function getCookie() {
+    const pairs = document.cookie.split('; ');
+    pairs.find(p => {
+        if(p === 'page=viewed') {
+            deleteWindow();
+        } else {
+            popUpWindow.className = 'modal modal_active'; // активация модального окна
+
+            closing.forEach((elem) => { // итерация по массиву closing
+                if (elem.className.includes('show-success')){ // содержит ли элемент с классом show-success
+                    elem.onclick = function() {
+                        popUpWindow.classList.remove('modal_active'); // удаление класса modal_active у предыдущего окна 
+                        modalSuccess.classList.add('modal_active');
+                        const btnSuccess = document.querySelector('.btn_success'); // доступ к зеленой кнопке во втором модальном окне
+                        btnSuccess.onclick = deleteWindow;
+                    };
+                } else {
+                    elem.onclick = deleteWindow;
+                };
+            });
+        }
+    })
 }
 
-closing.forEach((elem) => { // итерация по массиву closing
-    if (elem.className.includes('show-success')){ // содержит ли элемент с классом show-success
-        elem.onclick = function() {
-            popUpWindow.classList.remove('modal_active'); // удаление класса modal_active у предыдущего окна 
-            modalSuccess.classList.add('modal_active');
-            const btnSuccess = document.querySelector('.btn_success'); // доступ к зеленой кнопке во втором модальном окне
-            btnSuccess.onclick = deleteWindow;
-        };
-    } else {
-        elem.onclick = deleteWindow;
-    };
-});
+getCookie();
 
 function deleteWindow() {
     activeWindows.forEach((i) => i.classList.remove('modal_active'));
-    localStorage.modal = 'inactive';
+    document.cookie = `page=${encodeURIComponent('viewed')}`;
 }
